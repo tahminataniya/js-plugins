cdc855cd3f369339e7463079c0e6bc04d7e20ad8
tisagar11 <tariqulislamsagar.cse@gmail.com>
2022-11-26 00:54:17 +0600

    Slick slider completed

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 	$('.wrapper').slick({
 		infinite: true,
 	    slidesToShow: 3,
-	    slidesToScroll: 1 
-	    // dots:true
+	    slidesToScroll: 1, 
+	    dots:true,
+	    arrows:false
 	});
 
 });
@@ -16,28 +17,34 @@ $('.wrapper2').slick({
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
+  arrows:false,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        infinite: true,
-        dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 992,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       }
     }
     // You can unslick at a given breakpoint now by adding:
